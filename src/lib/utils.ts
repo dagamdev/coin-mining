2e48f7fe1f2625afeb55689d202e9a3ee00e5a84
Dagadev <gacub20ru@gmail.com>
2024-06-27 17:35:58 -0600

    Add formattedTime function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,22 @@ export function getLocalData (key: string, notString?: boolean) {
 export function saveLocalData <T=any> (key: string, data: T) {
   localStorage.setItem(key, typeof data === 'string' ? data : JSON.stringify(data))
 }
+
+export function formattedTime (time: number) {
+  const parts: string[] = []
+  const second = 1000, minute = second * 60, hour = minute * 60, day = hour * 24
+  const partTimes = [time / day, time % day / hour, time % hour / minute, time % minute / second]
+  let validPart = false
+
+  for (const partTime of partTimes) {
+    const part = Math.floor(partTime).toString().padStart(2, '0')
+
+    if (validPart === false && part !== '00') validPart = true
+
+    if (part !== '00' || validPart) {
+      parts.push(part)
+    }
+  }
+
+  return parts.length === 0 ? '0' : parts.join(':')
+}
